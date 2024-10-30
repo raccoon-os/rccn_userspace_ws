dfdd53a372e2383fe2702aab10344a15eb9ac73e
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-10-30 08:57:34 +0000

    WIP PusService

--- src/rccn_usr_example_app/src/app.rs
+++ src/rccn_usr_example_app/src/app.rs
@@ -1,5 +1,5 @@
-use rccn_usr::{time::TimestampHelper, types::VirtualChannelOutMap};
-use satrs::pus::verification::VerificationReporter;
+use rccn_usr::{r2r::ServiceRequest, time::TimestampHelper, types::VirtualChannelOutMap};
+use satrs::pus::verification::{VerificationReporter, VerificationReportingProvider};
 use spacepackets::{
     ecss::{
         tc::PusTcReader,
@@ -16,21 +16,61 @@ pub enum PusServiceError {
 type ServiceResult<T> = Result<T, PusServiceError>;
 
 #[derive(Clone)]
-pub struct PusServiceCommon {
+pub struct PusServiceCommon<'a> {
     pub apid: u16,
     pub verification_reporter: VerificationReporter,
-    pub virtual_channel_tx: VirtualChannelOutMap,
+    pub virtual_channel_tx: &'a VirtualChannelOutMap,
     pub timestamp_helper: TimestampHelper
 }
 
-impl PusServiceCommon {
-    fn handle_tc_bytes(&self, bytes: Vec<u8>) -> ServiceResult<()> {
-        let tc = match PusTcReader::new(&bytes) {
+
+pub enum CommandParseError {
+    Unknown()
+}
+
+type CommandParseResult<T> = Result<T, CommandParseError>;
+
+pub enum AcceptanceError {
+    PusError(PusError),
+    UnknownApid(u16),
+    UnknownService(u16),
+    UnknownSubservice(u16),
+    CommandParseError(CommandParseError),
+    ArgumentError(),
+    ServiceDisconnected()
+}
+
+type AcceptanceResult = Result<(), AcceptanceError>;
+
+pub trait ServiceCommand {
+    fn from_pus_tc(tc: &PusTcReader) -> CommandParseResult<Self> where Self: Sized;
+}
+
+pub trait PusService {
+    type CommandT: ServiceCommand;
+
+    fn get_service_common(&self) -> &mut PusServiceCommon;
+
+    fn handle_tc(&self, tc: &Self::CommandT) -> ServiceResult<()>;
+
+    fn handle_tc_bytes(&self, bytes: &[u8]) -> AcceptanceResult {
+        let common = self.get_service_common();
+
+        // TODO configure which TM channel to transmit on
+        let tm_sender = common.virtual_channel_tx.get(&0);
+
+        let pus_tc = match PusTcReader::new(&bytes) {
             Err(e) => {
                 // TODO send error telemetry
-                Err(PusServiceError::PusError(e))
+                //common.verification_reporter.acceptance_failure(sender, token, params)
+                Err(AcceptanceError::PusError(e))
             },
             Ok((tc, _size)) => Ok(tc)
+        }?;
+
+        let app_tc = match Self::CommandT::from_pus_tc(&pus_tc) {
+            Err(_) => todo!(),
+            Ok(tc) => Ok(tc)
         }?;
 
         // Check if the TC is destined for this application
