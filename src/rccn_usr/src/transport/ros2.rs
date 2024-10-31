c63fe3241de697a8720dfaa479022e301fd9ab4f
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-10-31 12:33:39 +0000

    Simplify transport/ros2, only one node is used now

--- src/rccn_usr/src/transport/ros2.rs
+++ src/rccn_usr/src/transport/ros2.rs
@@ -1,9 +1,13 @@
-use std::thread;
+use std::{
+    sync::{Arc, Mutex},
+    thread::{self},
+    time::Duration,
+};
 
 use async_std::stream::StreamExt;
 use crossbeam_channel::{Receiver, Select, Sender};
 use futures::{executor::LocalPool, task::SpawnExt};
-use r2r::{rccn_usr_msgs::msg::RawBytes, Publisher, QosProfile};
+use r2r::{rccn_usr_msgs::msg::RawBytes, QosProfile};
 use thiserror::Error;
 
 use super::{TransportHandler, TransportReader, TransportResult, TransportWriter};
@@ -23,9 +27,8 @@ pub enum Ros2ReaderConfig {
 }
 
 pub struct Ros2TransportHandler {
-    ctx: r2r::Context,
-    node: r2r::Node,
-    publishers: Vec<TransportWriter<Publisher<RawBytes>>>,
+    node: Arc<Mutex<r2r::Node>>,
+    publishers: Vec<TransportWriter<String>>,
     readers: Vec<TransportReader<Ros2ReaderConfig>>,
     name_prefix: String,
 }
@@ -33,15 +36,11 @@ pub struct Ros2TransportHandler {
 impl Ros2TransportHandler {
     pub fn new(name_prefix: String) -> Result<Self, Ros2TransportError> {
         let ctx = r2r::Context::create().map_err(Ros2TransportError::R2RError)?;
-        let node = r2r::Node::create(
-            ctx.clone(),
-            format!("{}_publishers", name_prefix).as_str(),
-            "/",
-        )
-        .map_err(Ros2TransportError::R2RError)?;
+        let node = Arc::new(Mutex::new(
+            r2r::Node::create(ctx, &name_prefix, "/").map_err(Ros2TransportError::R2RError)?,
+        ));
 
         Ok(Self {
-            ctx: ctx.clone(),
             node,
             publishers: Vec::new(),
             readers: Vec::new(),
@@ -58,15 +57,8 @@ impl TransportHandler for Ros2TransportHandler {
 
     fn add_transport_writer(&mut self, rx: Receiver<Vec<u8>>, config: Self::WriterConfig) {
         let topic: String = config;
-        let publisher = self
-            .node
-            .create_publisher::<RawBytes>(&topic, QosProfile::default())
-            .unwrap();
 
-        self.publishers.push(TransportWriter {
-            rx,
-            conf: publisher,
-        });
+        self.publishers.push(TransportWriter { rx, conf: topic });
     }
 
     fn add_transport_reader(&mut self, tx: Sender<Vec<u8>>, conf: Self::ReaderConfig) {
@@ -74,30 +66,50 @@ impl TransportHandler for Ros2TransportHandler {
     }
 
     fn run(self) -> TransportResult {
-        let _readers_handle = thread::spawn(move || {
-            run_ros2_readers(self.ctx.clone(), self.name_prefix, &self.readers)
+        let node_clone = self.node.clone();
+        let _readers_handle = thread::spawn(move || run_ros2_readers(node_clone, &self.readers));
+
+        let node_clone = self.node.clone();
+        let _spinner_handle = thread::spawn(move || loop {
+            node_clone
+                .lock()
+                .unwrap()
+                .spin_once(Duration::from_millis(100));
         });
+
         if self.publishers.len() == 0 {
-            return Ok(())
+            return Ok(());
         }
 
         let mut select = Select::new();
-        for TransportWriter { rx, conf: _ } in self.publishers.iter() {
+        let mut publishers = Vec::new();
+
+        for TransportWriter { rx, conf } in self.publishers.iter() {
             select.recv(rx);
+
+            let publisher = self
+                .node
+                .lock()
+                .unwrap()
+                .create_publisher::<RawBytes>(conf, QosProfile::default())
+                .unwrap();
+
+            publishers.push(publisher);
         }
 
         loop {
             let op = select.select();
             let index = op.index();
 
-            let TransportWriter { rx, conf: ros2_pub } = &self.publishers[index];
+            let TransportWriter { rx, conf: _ } = &self.publishers[index];
+            let publisher = &publishers[index];
             match op.recv(rx) {
                 Ok(data) => {
                     println!("Got data on channel {}, publishing to topic.", index);
 
                     let mut msg = RawBytes::default();
                     msg.data = data;
-                    match ros2_pub.publish(&msg) {
+                    match publisher.publish(&msg) {
                         Ok(()) => {
                             println!("Published successfully.")
                         }
@@ -112,20 +124,13 @@ impl TransportHandler for Ros2TransportHandler {
     }
 }
 
-fn run_ros2_readers(
-    ctx: r2r::Context,
-    name_prefix: String,
-    readers: &Vec<TransportReader<Ros2ReaderConfig>>,
-) {
+fn run_ros2_readers(node: Arc<Mutex<r2r::Node>>, readers: &Vec<TransportReader<Ros2ReaderConfig>>) {
     if readers.len() == 0 {
         return;
     }
 
     let mut pool = LocalPool::new();
     let spawner = pool.spawner();
-
-    let mut node =
-        r2r::Node::create(ctx, format!("{}_receivers", name_prefix).as_str(), "/").unwrap();
 
     for TransportReader { conf, tx } in readers.iter() {
         match conf {
@@ -134,8 +139,11 @@ fn run_ros2_readers(
                 let topic = topic.clone();
 
                 let mut subscription = node
+                    .lock()
+                    .unwrap()
                     .subscribe::<RawBytes>(&topic, QosProfile::default())
                     .unwrap();
+
                 let _ = spawner
                     .spawn(async move {
                         println!("Subscribed to {topic}.");
@@ -160,8 +168,5 @@ fn run_ros2_readers(
         }
     }
 
-    loop {
-        node.spin_once(std::time::Duration::from_millis(100));
-        pool.run_until_stalled();
-    }
+    pool.run();
 }
