4f978595e3dd7e5f80f7fb3634dad92ee7f86b0d
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-11-03 15:11:48 +0000

    Add WIP rccn_usr_gen crate

---
+++ src/rccn_usr_gen/src/lib.rs
@@ -0,0 +1,38 @@
+use std::path::Path;
+
+use xtce_rs::{mdb::MissionDatabase, parser};
+
+
+pub fn gen_parameters() -> MissionDatabase {
+    let files = [
+        "test_data/dt.xml",
+        "test_data/base.xml",
+        "test_data/parameters-dt-v2.xml",
+    ].map(Path::new);
+
+    parser::parse_files(&files).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use xtce_rs::mdb::{NamedItem, QualifiedName};
+
+    use super::*;
+
+    #[test]
+    fn test_gen_parameters() {
+        let mut mdb = gen_parameters();
+        let container = mdb.search_container("/PUS/pus-tm").unwrap();
+        let name_db = &mdb.name_db().clone();
+
+        let qualified_name = QualifiedName::from_str(name_db, "/parameters-dt").unwrap();
+        let sys = mdb.get_space_system(&qualified_name).unwrap();
+
+        for (name, idx) in sys.parameters.iter() {
+            let param = mdb.get_parameter(*idx);
+            let name = mdb.name2str(param.name());
+            println!("Param {:?}", name);
+
+        }
+    }
+}
