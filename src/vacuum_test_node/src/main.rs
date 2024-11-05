890a8b1786dcb23ca8cc892608cb9a3c02691b4e
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-11-05 19:01:33 +0000

    vacuum_test_node: use new PusApp API

--- src/vacuum_test_node/src/main.rs
+++ src/vacuum_test_node/src/main.rs
@@ -1,49 +1,19 @@
 use anyhow::Result;
-use rccn_usr::{
-    config::VirtualChannel,
-    service::{AcceptanceError, PusService},
-    transport::{ros2::new_shared_ros2_node, TransportManager},
-};
+use rccn_usr::{config::VirtualChannel, transport::ros2::new_shared_ros2_node};
+use rccn_usr_pus::app::PusApp;
 use stress_service::service::StressTestService;
 
 mod stress_service;
 
 fn main() -> Result<()> {
     let node = new_shared_ros2_node("vacuum_test_node", &"/")?;
+    let mut app = PusApp::new_with_ros2_node(42, node.clone());
 
-    let mut transport_manager = TransportManager::new_with_ros2_node(node.clone())?;
-    transport_manager.add_virtual_channel(&VirtualChannel::on_ros2_topic(0, "bus_realtime"))?;
-    let ((vc_tx_map, mut vc_rx_map), transport_handles) = transport_manager.run();
+    app.add_virtual_channel(&VirtualChannel::on_ros2_topic(0, "bus_realtime"))?;
 
-    let mut stress_service = StressTestService::new(node.clone());
+    let stress_service = StressTestService::new(node.clone());
+    app.register_service(stress_service);
 
-    // Process incoming TCs from the virtual channel
-    let tc_receiver = vc_rx_map.remove(&0).expect("VC 0 TC receiver not found");
-    loop {
-        match tc_receiver.recv() {
-            Ok(bytes) => match stress_service.handle_tc_bytes(&bytes) {
-                Ok(_) => {
-                    println!("Command handled succesfully.");
-                }
-                Err(AcceptanceError::UnknownApid(apid)) => {
-                    println!("Command was for APID {apid}, ignoring.");
-                }
-                Err(e) => {
-                    println!("Error handling command: {e:?}");
-                }
-            },
-            Err(e) => {
-                println!("TC RX channel closed, exiting. {e:?}");
-                break;
-            }
-        }
-    }
-
-    // Wait for transport threads to complete
-    for handle in transport_handles {
-        handle.join().unwrap()?;
-    }
-
-    println!("Closing down.");
+    app.run();
     Ok(())
 }
