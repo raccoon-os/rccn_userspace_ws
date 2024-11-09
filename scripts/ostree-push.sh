2c616ec19929130a10459ad524d9375d943cfaf1
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-11-09 23:04:42 +0100

    Init ostree in archive mode

--- scripts/ostree-push.sh
+++ scripts/ostree-push.sh
@@ -6,19 +6,20 @@ BRANCH="$2"
 
 ostree init \
     --repo=repo \
-    --mode=bare-user
+    --mode=archive
 
 # Create the desired filesystem structure
 mkdir -p ./ostree_root/usr/lib/$PACKAGE_NAME/
 cp -r ./install ./ostree_root/usr/lib/$PACKAGE_NAME/
 
 # Add the build output files
-ostree commit \
+REF=$(ostree commit \
     --repo=repo \
     --branch="rccn_usr_ws/$PLATFORM/$BRANCH" \
-    --tree=dir=./ostree_root
+    --tree=dir=./ostree_root)
 
 # Push to server
 ostree-push \
     --repo repo \
-    builder@localhost:/home/builder/meta-raccoon-mission/build/tmp/deploy/images/phyboard-pollux-imx8mp-3/ostree_repo
+    builder@localhost:/home/builder/meta-raccoon-mission/build/tmp/deploy/images/phyboard-pollux-imx8mp-3/ostree_repo \
+    $REF
