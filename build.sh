469631b96296275d165d55b80c0c2d00f73480df
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-11-09 09:08:10 +0000

    Debug colcon-ros-cargo

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 source ~/.rccnenv
 
+~/.local/bin/uv pip install git+https://github.com/jdiez17/colcon-ros-cargo
+
 colcon build \
     --event-handlers console_direct+ \
-    --packages-up-to vacuum_test_node \
+    --packages-up-to vacuum_test_node
+
