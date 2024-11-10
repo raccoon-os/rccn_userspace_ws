759a617cdb64f2c6b0e1fbfd5141f8241a26c6fb
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-11-10 17:16:17 +0000

    Reduce build time by limiting IDL packages

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ source ~/.rccnenv
 
 ~/.local/bin/uv pip install git+https://github.com/jdiez17/colcon-ros-cargo
 
+export IDL_PACKAGE_FILTER="std_msgs;rccn_usr_msgs"
+
 colcon build \
     --event-handlers console_direct+ \
     --packages-up-to vacuum_test_node \
