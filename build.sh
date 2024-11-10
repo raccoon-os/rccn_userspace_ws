0c99ef8e7ae436c3e9e0d42b1962d1d209be5ae4
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-11-10 17:18:02 +0000

    Include thermal_test_msgs

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ source ~/.rccnenv
 
 ~/.local/bin/uv pip install git+https://github.com/jdiez17/colcon-ros-cargo
 
-export IDL_PACKAGE_FILTER="std_msgs;rccn_usr_msgs"
+export IDL_PACKAGE_FILTER="std_msgs;rccn_usr_msgs;thermal_test_msgs"
 
 colcon build \
     --event-handlers console_direct+ \
